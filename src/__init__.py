f047482f9f72f9a7ee58d466e7049eb62370705f
Francesco215 <f.sacco@protonmail.com>
2022-10-08 15:03:50 +0200

    copied lucidrains source code

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from .data_manipulation import *
+from .unet import *
---
+++ src/data_manipulation.py
@@ -0,0 +1,452 @@
+import math
+from pathlib import Path
+from random import random
+from functools import partial
+from multiprocessing import cpu_count
+
+import torch
+from torch import nn, einsum
+from torch.special import expm1
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from torch.optim import Adam
+from torchvision import transforms as T, utils
+
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange
+
+from PIL import Image
+from tqdm.auto import tqdm
+from ema_pytorch import EMA
+
+from accelerate import Accelerator
+
+from .unet import *
+
+# convert to bit representations and back
+
+def decimal_to_bits(x, bits = BITS):
+    """ expects image tensor ranging from 0 to 1, outputs bit tensor ranging from -1 to 1 """
+    device = x.device
+
+    x = (x * 255).int().clamp(0, 255)
+
+    mask = 2 ** torch.arange(bits - 1, -1, -1, device = device)
+    mask = rearrange(mask, 'd -> d 1 1')
+    x = rearrange(x, 'b c h w -> b c 1 h w')
+
+    bits = ((x & mask) != 0).float()
+    bits = rearrange(bits, 'b c d h w -> b (c d) h w')
+    bits = bits * 2 - 1
+    return bits
+
+def bits_to_decimal(x, bits = BITS):
+    """ expects bits from -1 to 1, outputs image tensor from 0 to 1 """
+    device = x.device
+
+    x = (x > 0).int()
+    mask = 2 ** torch.arange(bits - 1, -1, -1, device = device, dtype = torch.int32)
+
+    mask = rearrange(mask, 'd -> d 1 1')
+    x = rearrange(x, 'b (c d) h w -> b c d h w', d = 8)
+    dec = reduce(x * mask, 'b c d h w -> b c h w', 'sum')
+    return (dec / 255).clamp(0., 1.)
+
+# bit diffusion class
+
+def log(t, eps = 1e-20):
+    return torch.log(t.clamp(min = eps))
+
+def right_pad_dims_to(x, t):
+    padding_dims = x.ndim - t.ndim
+    if padding_dims <= 0:
+        return t
+    return t.view(*t.shape, *((1,) * padding_dims))
+
+def beta_linear_log_snr(t):
+    return -torch.log(expm1(1e-4 + 10 * (t ** 2)))
+
+def alpha_cosine_log_snr(t, s: float = 0.008):
+    return -log((torch.cos((t + s) / (1 + s) * math.pi * 0.5) ** -2) - 1, eps = 1e-5) # not sure if this accounts for beta being clipped to 0.999 in discrete version
+
+def log_snr_to_alpha_sigma(log_snr):
+    return torch.sqrt(torch.sigmoid(log_snr)), torch.sqrt(torch.sigmoid(-log_snr))
+
+class BitDiffusion(nn.Module):
+    def __init__(
+        self,
+        model,
+        *,
+        image_size,
+        timesteps = 1000,
+        use_ddim = False,
+        noise_schedule = 'cosine',
+        time_difference = 0.,
+        bit_scale = 1.
+    ):
+        super().__init__()
+        self.model = model
+        self.channels = self.model.channels
+
+        self.image_size = image_size
+
+        if noise_schedule == "linear":
+            self.log_snr = beta_linear_log_snr
+        elif noise_schedule == "cosine":
+            self.log_snr = alpha_cosine_log_snr
+        else:
+            raise ValueError(f'invalid noise schedule {noise_schedule}')
+
+        self.bit_scale = bit_scale
+
+        self.timesteps = timesteps
+        self.use_ddim = use_ddim
+
+        # proposed in the paper, summed to time_next
+        # as a way to fix a deficiency in self-conditioning and lower FID when the number of sampling timesteps is < 400
+
+        self.time_difference = time_difference
+
+    @property
+    def device(self):
+        return next(self.model.parameters()).device
+
+    def get_sampling_timesteps(self, batch, *, device):
+        times = torch.linspace(1., 0., self.timesteps + 1, device = device)
+        times = repeat(times, 't -> b t', b = batch)
+        times = torch.stack((times[:, :-1], times[:, 1:]), dim = 0)
+        times = times.unbind(dim = -1)
+        return times
+
+    @torch.no_grad()
+    def ddpm_sample(self, shape, time_difference = None):
+        batch, device = shape[0], self.device
+
+        time_difference = default(time_difference, self.time_difference)
+
+        time_pairs = self.get_sampling_timesteps(batch, device = device)
+
+        img = torch.randn(shape, device=device)
+
+        x_start = None
+
+        for time, time_next in tqdm(time_pairs, desc = 'sampling loop time step', total = self.timesteps):
+
+            # add the time delay
+
+            time_next = (time_next - self.time_difference).clamp(min = 0.)
+
+            noise_cond = self.log_snr(time)
+
+            # get predicted x0
+
+            x_start = self.model(img, noise_cond, x_start)
+
+            # clip x0
+
+            x_start.clamp_(-self.bit_scale, self.bit_scale)
+
+            # get log(snr)
+
+            log_snr = self.log_snr(time)
+            log_snr_next = self.log_snr(time_next)
+            log_snr, log_snr_next = map(partial(right_pad_dims_to, img), (log_snr, log_snr_next))
+
+            # get alpha sigma of time and next time
+
+            alpha, sigma = log_snr_to_alpha_sigma(log_snr)
+            alpha_next, sigma_next = log_snr_to_alpha_sigma(log_snr_next)
+
+            # derive posterior mean and variance
+
+            c = -expm1(log_snr - log_snr_next)
+
+            mean = alpha_next * (img * (1 - c) / alpha + c * x_start)
+            variance = (sigma_next ** 2) * c
+            log_variance = log(variance)
+
+            # get noise
+
+            noise = torch.where(
+                rearrange(time_next > 0, 'b -> b 1 1 1'),
+                torch.randn_like(img),
+                torch.zeros_like(img)
+            )
+
+            img = mean + (0.5 * log_variance).exp() * noise
+
+        return bits_to_decimal(img)
+
+    @torch.no_grad()
+    def ddim_sample(self, shape, time_difference = None):
+        batch, device = shape[0], self.device
+
+        time_difference = default(time_difference, self.time_difference)
+
+        time_pairs = self.get_sampling_timesteps(batch, device = device)
+
+        img = torch.randn(shape, device = device)
+
+        x_start = None
+
+        for times, times_next in tqdm(time_pairs, desc = 'sampling loop time step'):
+
+            # get times and noise levels
+
+            log_snr = self.log_snr(times)
+            log_snr_next = self.log_snr(times_next)
+
+            padded_log_snr, padded_log_snr_next = map(partial(right_pad_dims_to, img), (log_snr, log_snr_next))
+
+            alpha, sigma = log_snr_to_alpha_sigma(padded_log_snr)
+            alpha_next, sigma_next = log_snr_to_alpha_sigma(padded_log_snr_next)
+
+            # add the time delay
+
+            times_next = (times_next - time_difference).clamp(min = 0.)
+
+            # predict x0
+
+            x_start = self.model(img, log_snr, x_start)
+
+            # clip x0
+
+            x_start.clamp_(-self.bit_scale, self.bit_scale)
+
+            # get predicted noise
+
+            pred_noise = (img - alpha * x_start) / sigma.clamp(min = 1e-8)
+
+            # calculate x next
+
+            img = x_start * alpha_next + pred_noise * sigma_next
+
+        return bits_to_decimal(img)
+
+    @torch.no_grad()
+    def sample(self, batch_size = 16):
+        image_size, channels = self.image_size, self.channels
+        sample_fn = self.ddpm_sample if not self.use_ddim else self.ddim_sample
+        return sample_fn((batch_size, channels, image_size, image_size))
+
+    def forward(self, img, *args, **kwargs):
+        batch, c, h, w, device, img_size, = *img.shape, img.device, self.image_size
+        assert h == img_size and w == img_size, f'height and width of image must be {img_size}'
+
+        # sample random times
+
+        times = torch.zeros((batch,), device = device).float().uniform_(0, 0.999)
+
+        # convert image to bit representation
+
+        img = decimal_to_bits(img) * self.bit_scale
+
+        # noise sample
+
+        noise = torch.randn_like(img)
+
+        noise_level = self.log_snr(times)
+        padded_noise_level = right_pad_dims_to(img, noise_level)
+        alpha, sigma =  log_snr_to_alpha_sigma(padded_noise_level)
+
+        noised_img = alpha * img + sigma * noise
+
+        # if doing self-conditioning, 50% of the time, predict x_start from current set of times
+        # and condition with unet with that
+        # this technique will slow down training by 25%, but seems to lower FID significantly
+
+        self_cond = None
+        if random() < 0.5:
+            with torch.no_grad():
+                self_cond = self.model(noised_img, noise_level).detach_()
+
+        # predict and take gradient step
+
+        pred = self.model(noised_img, noise_level, self_cond)
+
+        return F.mse_loss(pred, img)
+
+# dataset classes
+
+class Dataset(Dataset):
+    def __init__(
+        self,
+        folder,
+        image_size,
+        exts = ['jpg', 'jpeg', 'png', 'tiff'],
+        augment_horizontal_flip = False,
+        convert_image_to = None
+    ):
+        super().__init__()
+        self.folder = folder
+        self.image_size = image_size
+        self.paths = [p for ext in exts for p in Path(f'{folder}').glob(f'**/*.{ext}')]
+
+        maybe_convert_fn = partial(convert_image_to, convert_image_to) if exists(convert_image_to) else nn.Identity()
+
+        self.transform = T.Compose([
+            T.Lambda(maybe_convert_fn),
+            T.Resize(image_size),
+            T.RandomHorizontalFlip() if augment_horizontal_flip else nn.Identity(),
+            T.CenterCrop(image_size),
+            T.ToTensor()
+        ])
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(path)
+        return self.transform(img)
+
+# trainer class
+
+class Trainer(object):
+    def __init__(
+        self,
+        diffusion_model,
+        folder,
+        *,
+        train_batch_size = 16,
+        gradient_accumulate_every = 1,
+        augment_horizontal_flip = True,
+        train_lr = 1e-4,
+        train_num_steps = 100000,
+        ema_update_every = 10,
+        ema_decay = 0.995,
+        adam_betas = (0.9, 0.99),
+        save_and_sample_every = 1000,
+        num_samples = 25,
+        results_folder = './results',
+        amp = False,
+        fp16 = False,
+        split_batches = True,
+        convert_image_to = None
+    ):
+        super().__init__()
+
+        self.accelerator = Accelerator(
+            split_batches = split_batches,
+            mixed_precision = 'fp16' if fp16 else 'no'
+        )
+
+        self.accelerator.native_amp = amp
+
+        self.model = diffusion_model
+
+        assert has_int_squareroot(num_samples), 'number of samples must have an integer square root'
+        self.num_samples = num_samples
+        self.save_and_sample_every = save_and_sample_every
+
+        self.batch_size = train_batch_size
+        self.gradient_accumulate_every = gradient_accumulate_every
+
+        self.train_num_steps = train_num_steps
+        self.image_size = diffusion_model.image_size
+
+        # dataset and dataloader
+
+        self.ds = Dataset(folder, self.image_size, augment_horizontal_flip = augment_horizontal_flip, convert_image_to = convert_image_to)
+        dl = DataLoader(self.ds, batch_size = train_batch_size, shuffle = True, pin_memory = True, num_workers = cpu_count())
+
+        dl = self.accelerator.prepare(dl)
+        self.dl = cycle(dl)
+
+        # optimizer
+
+        self.opt = Adam(diffusion_model.parameters(), lr = train_lr, betas = adam_betas)
+
+        # for logging results in a folder periodically
+
+        if self.accelerator.is_main_process:
+            self.ema = EMA(diffusion_model, beta = ema_decay, update_every = ema_update_every)
+
+            self.results_folder = Path(results_folder)
+            self.results_folder.mkdir(exist_ok = True)
+
+        # step counter state
+
+        self.step = 0
+
+        # prepare model, dataloader, optimizer with accelerator
+
+        self.model, self.opt = self.accelerator.prepare(self.model, self.opt)
+
+    def save(self, milestone):
+        if not self.accelerator.is_local_main_process:
+            return
+
+        data = {
+            'step': self.step,
+            'model': self.accelerator.get_state_dict(self.model),
+            'opt': self.opt.state_dict(),
+            'ema': self.ema.state_dict(),
+            'scaler': self.accelerator.scaler.state_dict() if exists(self.accelerator.scaler) else None
+        }
+
+        torch.save(data, str(self.results_folder / f'model-{milestone}.pt'))
+
+    def load(self, milestone):
+        data = torch.load(str(self.results_folder / f'model-{milestone}.pt'))
+
+        model = self.accelerator.unwrap_model(self.model)
+        model.load_state_dict(data['model'])
+
+        self.step = data['step']
+        self.opt.load_state_dict(data['opt'])
+        self.ema.load_state_dict(data['ema'])
+
+        if exists(self.accelerator.scaler) and exists(data['scaler']):
+            self.accelerator.scaler.load_state_dict(data['scaler'])
+
+    def train(self):
+        accelerator = self.accelerator
+        device = accelerator.device
+
+        with tqdm(initial = self.step, total = self.train_num_steps, disable = not accelerator.is_main_process) as pbar:
+
+            while self.step < self.train_num_steps:
+
+                total_loss = 0.
+
+                for _ in range(self.gradient_accumulate_every):
+                    data = next(self.dl).to(device)
+
+                    with self.accelerator.autocast():
+                        loss = self.model(data)
+                        loss = loss / self.gradient_accumulate_every
+                        total_loss += loss.item()
+
+                    self.accelerator.backward(loss)
+
+                pbar.set_description(f'loss: {total_loss:.4f}')
+
+                accelerator.wait_for_everyone()
+
+                self.opt.step()
+                self.opt.zero_grad()
+
+                accelerator.wait_for_everyone()
+
+                if accelerator.is_main_process:
+                    self.ema.to(device)
+                    self.ema.update()
+
+                    if self.step != 0 and self.step % self.save_and_sample_every == 0:
+                        self.ema.ema_model.eval()
+
+                        with torch.no_grad():
+                            milestone = self.step // self.save_and_sample_every
+                            batches = num_to_groups(self.num_samples, self.batch_size)
+                            all_images_list = list(map(lambda n: self.ema.ema_model.sample(batch_size=n), batches))
+
+                        all_images = torch.cat(all_images_list, dim = 0)
+                        utils.save_image(all_images, str(self.results_folder / f'sample-{milestone}.png'), nrow = int(math.sqrt(self.num_samples)))
+                        self.save(milestone)
+
+                self.step += 1
+                pbar.update(1)
+
+        accelerator.print('training complete')
